9df998b0fec24089d3d7eb1d393b3625199f29cd
narengavli <u20ec050@eced.svnit.ac.in>
2023-04-05 13:03:52 +0530

    add certificates

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 // Fetch Data
 const ProjectList = require("./data/ProjectList.json");
 const Skills = require("./data/skills.json");
+const Certificates = require("./data/Certificate.json");
 
 app.get('/projects/:tech', (req, res) => {
     const tech = req.params.tech.toLowerCase();
@@ -17,6 +18,10 @@ app.get('/projects/:tech', (req, res) => {
 
 app.get("/projects", (req, res) => {
     res.send(ProjectList);
+});
+
+app.get("/certificates", (req, res) => {
+    res.send(Certificates);
 });
 
 app.get("/skills", (req, res) => {
